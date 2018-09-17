5240ae13c3c935a99bdaffbf94ef4ef5007cb369
Josh Schwartzman <josh@dreamseedo.org>
2018-09-17 14:11:03 -0700

    Try using has_one relationship

--- lib/network-api/subscription.rb
+++ lib/network-api/subscription.rb
@@ -1,4 +1,8 @@
 module NetworkApi
   class Subscription < Base
+    # Use has_one instead of belongs_to, which is a nested resource
+    has_one :payment_method
+    has_one :coupon
+    has_one :plan
   end
 end
--- lib/network-api/user.rb
+++ lib/network-api/user.rb
@@ -1,4 +1,5 @@
 module NetworkApi
   class User < Base
+    has_many :emails
   end
 end
