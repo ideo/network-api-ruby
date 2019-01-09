2ab1a1fba8b9cd305b75784d066827f253ac28cd
Josh Schwartzman <josh@dreamseedo.org>
2019-01-09 11:21:26 -0800

    Add typecasting for AppMetric

--- lib/network-api/base.rb
+++ lib/network-api/base.rb
@@ -7,6 +7,7 @@ module NetworkApi
     # There is a bug with included resources where ID is cast as an integer,
     # and then the resource can't be auto-linked
     property :id, type: :string
+    property :number, type: :float
 
     def self.configure(url:, api_token:, client_id:)
       self.site = url # e.g. https://profile.ideo.com/api/v1/
