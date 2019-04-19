5f0363bd4e45dae47493156c85351911aa357b57
Josh Schwartzman <josh@dreamseedo.org>
2019-04-19 14:17:19 -0700

    Change authentication to use named parameters

--- lib/network-api/authentication.rb
+++ lib/network-api/authentication.rb
@@ -2,26 +2,38 @@ module NetworkApi
   class Authentication
     # URL to sign in
     def self.sign_in_url(redirect_url:, cookies:, addtl_params: {})
-      uri = uri_with_oauth_params(redirect_url, cookies, addtl_params)
+      uri = uri_with_oauth_params(
+        redirect_url: redirect_url,
+        cookies: cookies,
+        addtl_params: addtl_params
+      )
       uri.path = '/users/sign_in'
       uri.to_s
     end
 
     # URL to create a new account
     def self.sign_up_url(redirect_url:, cookies:, addtl_params: {})
-      uri = uri_with_oauth_params(redirect_url, cookies, addtl_params)
+      uri = uri_with_oauth_params(
+        redirect_url: redirect_url,
+        cookies: cookies,
+        addtl_params: addtl_params
+      )
       uri.path = '/users/sign_up'
       uri.to_s
     end
 
     # URL to sign in with a specific provider, e.g. ford
     def self.provider_auth_url(provider:, redirect_url:, cookies:, addtl_params: {})
-      uri = uri_with_oauth_params(redirect_url, cookies, addtl_params)
+      uri = uri_with_oauth_params(
+        redirect_url: redirect_url,
+        cookies: cookies,
+        addtl_params: addtl_params
+      )
       uri.path = "/identity_providers/#{provider}/auth"
       uri.to_s
     end
 
-    def self.uri_with_oauth_params(redirect_url, cookies, addtl_params = {})
+    def self.uri_with_oauth_params(redirect_url:, cookies:, addtl_params: {})
       uri = URI(NetworkApi::Base.site)
       uri.query = addtl_params.merge(
         client_id: NetworkApi::Base.client_id,
