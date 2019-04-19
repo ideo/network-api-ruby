d53764dcc5bbc692ae5e629a05a2fdd638e18c51
Josh Schwartzman <josh@dreamseedo.org>
2019-04-19 14:12:37 -0700

    Support addtl query parameters for auth

--- lib/network-api/authentication.rb
+++ lib/network-api/authentication.rb
@@ -1,34 +1,34 @@
 module NetworkApi
   class Authentication
     # URL to sign in
-    def self.sign_in_url(redirect_url:, cookies:)
-      uri = uri_with_oauth_params(redirect_url, cookies)
+    def self.sign_in_url(redirect_url:, cookies:, addtl_params: {})
+      uri = uri_with_oauth_params(redirect_url, cookies, addtl_params)
       uri.path = '/users/sign_in'
       uri.to_s
     end
 
     # URL to create a new account
-    def self.sign_up_url(redirect_url:, cookies:)
-      uri = uri_with_oauth_params(redirect_url, cookies)
+    def self.sign_up_url(redirect_url:, cookies:, addtl_params: {})
+      uri = uri_with_oauth_params(redirect_url, cookies, addtl_params)
       uri.path = '/users/sign_up'
       uri.to_s
     end
 
     # URL to sign in with a specific provider, e.g. ford
-    def self.provider_auth_url(provider:, redirect_url:, cookies:)
-      uri = uri_with_oauth_params(redirect_url, cookies)
+    def self.provider_auth_url(provider:, redirect_url:, cookies:, addtl_params: {})
+      uri = uri_with_oauth_params(redirect_url, cookies, addtl_params)
       uri.path = "/identity_providers/#{provider}/auth"
       uri.to_s
     end
 
-    def self.uri_with_oauth_params(redirect_url, cookies)
+    def self.uri_with_oauth_params(redirect_url, cookies, addtl_params = {})
       uri = URI(NetworkApi::Base.site)
-      uri.query = {
+      uri.query = addtl_params.merge(
         client_id: NetworkApi::Base.client_id,
         redirect_uri: redirect_url,
         response_type: 'code',
         state: NetworkApi::Base.state(cookies)
-      }.to_query
+      ).to_query
       uri
     end
   end
--- spec/network-api/authentication_spec.rb
+++ spec/network-api/authentication_spec.rb
@@ -14,12 +14,14 @@ describe NetworkApi::Authentication do
   let(:uri) { URI.parse(url) }
   let(:query_params) { CGI.parse(uri.query) }
   let(:redirect_url) { 'https://www.shape.space' }
+  let(:addtl_params) { {} }
 
   describe '#sign_in_url' do
     let(:url) do
       subject.sign_in_url(
         redirect_url: redirect_url,
-        cookies: cookies
+        cookies: cookies,
+        addtl_params: addtl_params
       )
     end
 
@@ -31,13 +33,26 @@ describe NetworkApi::Authentication do
     it 'has valid oauth params' do
       expect_oauth_query_params_to_match
     end
+
+    context 'with addtl_params' do
+      let!(:addtl_params) {
+        {
+          auth_token: 'something'
+        }
+      }
+
+      it 'adds them to query parameters' do
+        expect(query_params['auth_token']).to eq(['something'])
+      end
+    end
   end
 
   describe '#sign_up_url' do
     let(:url) do
       subject.sign_up_url(
         redirect_url: redirect_url,
-        cookies: cookies
+        cookies: cookies,
+        addtl_params: addtl_params
       )
     end
 
@@ -49,6 +64,18 @@ describe NetworkApi::Authentication do
     it 'has valid oauth params' do
       expect_oauth_query_params_to_match
     end
+
+    context 'with addtl_params' do
+      let!(:addtl_params) {
+        {
+          auth_token: 'something'
+        }
+      }
+
+      it 'adds them to query parameters' do
+        expect(query_params['auth_token']).to eq(['something'])
+      end
+    end
   end
 
   describe '#provider_auth_url' do
@@ -56,7 +83,8 @@ describe NetworkApi::Authentication do
       subject.provider_auth_url(
         provider: 'okta',
         redirect_url: redirect_url,
-        cookies: cookies
+        cookies: cookies,
+        addtl_params: addtl_params
       )
     end
 
@@ -64,6 +92,18 @@ describe NetworkApi::Authentication do
       expect(uri.host).to eq('profile.ideo.com')
       expect(uri.path).to eq('/identity_providers/okta/auth')
       expect_oauth_query_params_to_match
+    end
+
+    context 'with addtl_params' do
+      let!(:addtl_params) {
+        {
+          auth_token: 'something'
+        }
+      }
+
+      it 'adds them to query parameters' do
+        expect(query_params['auth_token']).to eq(['something'])
+      end
     end
 
     context 'with existing state' do
