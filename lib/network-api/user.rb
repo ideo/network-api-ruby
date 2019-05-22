652625087411194c5b03ff0a825643da343884ca
kaptron <dkaplan@ideo.com>
2019-05-22 15:57:35 -0700

    Adds support for generate_auth_token endpoint

--- lib/network-api/user.rb
+++ lib/network-api/user.rb
@@ -1,5 +1,7 @@
 module NetworkApi
   class User < Base
+    custom_endpoint :generate_auth_token, on: :member, request_method: :get
+
     has_many :emails
     has_many :applications
     has_one :location
