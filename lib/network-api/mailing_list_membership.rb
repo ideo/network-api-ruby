06cc37b1e8ca82d3492a5900966105e23d7e2215
Josh Schwartzman <josh@dreamseedo.org>
2019-05-31 16:30:24 -0700

    Set shallow relationship for mailing list membership

--- lib/network-api/mailing_list_membership.rb
+++ lib/network-api/mailing_list_membership.rb
@@ -1,6 +1,6 @@
 module NetworkApi
   class MailingListMembership < Base
-    belongs_to :mailing_list
+    belongs_to :mailing_list, shallow: true
     has_one :user
     has_one :organization
   end
