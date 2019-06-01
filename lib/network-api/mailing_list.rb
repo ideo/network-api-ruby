1a88b725ae0a592c5ebd72ba3b7aed48315c85c7
Josh Schwartzman <josh@dreamseedo.org>
2019-05-31 17:16:36 -0700

    Add batch API endpoint

--- lib/network-api/mailing_list.rb
+++ lib/network-api/mailing_list.rb
@@ -2,5 +2,12 @@ module NetworkApi
   class MailingList < Base
     has_one :application
     has_many :mailing_list_memberships
+
+    # Easily subscribe/update users for a mailing list using this endpoint
+    #
+    # Params:
+    #   user_uids (Array)
+    #   organization_id (Integer)
+    custom_endpoint :batch_create_or_update_memberships, on: :member, request_method: :post
   end
 end
