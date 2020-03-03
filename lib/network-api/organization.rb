a196ad7bd3e183064d6f38d85124a908320ddbcd
kaptron <dkaplan@ideo.com>
2020-03-03 15:22:39 -0800

    removes note about organization external_id

--- lib/network-api/organization.rb
+++ lib/network-api/organization.rb
@@ -5,13 +5,12 @@ module NetworkApi
     has_many :applications
     has_many :application_organizations
 
-    # TODO: deprecate this field?
     property :external_id, type: :string
 
     def self.find_by_external_id(external_id)
       where(
         client_id: client_id,
-        external_id: external_id
+        external_id: external_id,
       ).first
     end
   end
